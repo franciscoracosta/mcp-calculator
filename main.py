d83aeb9ca08bdb368deeb7aa6e27973b4801b50a
Francisco  Costa <francisco.rac91@gmail.com>
2025-05-13 23:32:51 +0100

    wip

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def get_greeting(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-   mcp.run(transport="streamable-http", host="0.0.0.0", port=8000, path="/mcp")
+   mcp.run()
